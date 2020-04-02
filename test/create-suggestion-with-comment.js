65b860120c2455239be3ef09a9e0808797b93e03
awilby <awilby@ucsd.edu>
2020-04-02 22:31:06 +1100

    tests from postman for creating suggestions

---
+++ test/create-suggestion-with-comment.js
@@ -0,0 +1,36 @@
+var http = require('follow-redirects').http;
+var fs = require('fs');
+
+var options = {
+  'method': 'POST',
+  'hostname': 'localhost',
+  'port': 8080,
+  'path': '/suggest',
+  'headers': {
+    'Content-Type': 'application/json'
+  },
+  'maxRedirects': 20
+};
+
+var req = http.request(options, function (res) {
+  var chunks = [];
+
+  res.on("data", function (chunk) {
+    chunks.push(chunk);
+  });
+
+  res.on("end", function (chunk) {
+    var body = Buffer.concat(chunks);
+    console.log(body.toString());
+  });
+
+  res.on("error", function (error) {
+    console.error(error);
+  });
+});
+
+var postData = JSON.stringify({"contact_name":"antonella","contact_email":"email@test.co","issue_target":"amazon","issue_summary":"packaging","issue_description":"test hello","comments":"hi there"});
+
+req.write(postData);
+
+req.end();
