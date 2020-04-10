694e00f5141a630d3290dda6879284a920ec4d35
awilby <awilby@ucsd.edu>
2020-04-10 13:37:27 +1000

    how the f do golang package imports work

--- api/config/db.go
+++ api/config/db.go
@@ -6,7 +6,8 @@ import (
             "fmt"
             "github.com/jinzhu/gorm"
             _ "github.com/jinzhu/gorm/dialects/postgres"
-            "github.com/GoogleCloudPlatform/cloudsql-proxy/cmd/cloud_sql_proxy"
+            //"github.com/GoogleCloudPlatform/cloudsql-proxy/cmd/cloud_sql_proxy"
+            "github.com/GoogleCloudPlatform/cloudsql-proxy/proxy/dialers/postgres"
             "github.com/act-up/backend/api/models"
 )
 
